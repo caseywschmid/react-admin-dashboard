08d5deceb24cf8d18b9835624a70366e10f1578c
Casey Schmid <caseywschmid@gmail.com>
2023-01-16 14:17:04 +0100

    Fixed issue with profile not loading

--- src/scenes/global/SideBar.jsx
+++ src/scenes/global/SideBar.jsx
@@ -89,7 +89,7 @@ const SideBar = () => {
             <Box mb="25px">
               <Box display="flex" justifyContent="center" alignItems="center">
                 <img
-                  src={"/profile.png"}
+                  src={require(`./profile.png`)}
                   alt="profile-user"
                   width="100px"
                   height="100px"
